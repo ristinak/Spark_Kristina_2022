4f28f87546fb088c1724a392da70677c938900bd
ristinak <ristinak@gmail.com>
2022-07-18 19:48:30 +0200

    Exercise done.

--- src/main/scala/com/github/ristinak/Day17HelloSpark.scala
+++ src/main/scala/com/github/ristinak/Day17HelloSpark.scala
@@ -14,8 +14,11 @@ object Day17HelloSpark extends App {
   divisibleBy5.show(10) //show first 10 rows
 
   //TODO create range of numbers 0 to 100
+  val myRange100 = spark.range(100).toDF("number")
   //TODO filter into numbers divisible by 10
+  val divisibleBy10 = myRange.where("number % 10 = 0")
   //TODO show the results
+  divisibleBy10.show(10)
 
   spark.stop() //or .close() if you want to stop the Spark engine before the program stops running
 }
