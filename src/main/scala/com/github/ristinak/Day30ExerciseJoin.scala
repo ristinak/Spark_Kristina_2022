0406343543a88f965c9b253cd1801353ff7d3431
ristinak <ristinak@gmail.com>
2022-08-13 10:58:37 +0300

    Day30 cube, join, pivot. Edited the customer.csv

--- src/main/scala/com/github/ristinak/Day30ExerciseJoin.scala
+++ src/main/scala/com/github/ristinak/Day30ExerciseJoin.scala
@@ -1,6 +1,8 @@
 package com.github.ristinak
 
 import com.github.ristinak.SparkUtil.{getSpark, readDataWithView}
+import jdk.jfr.Description
+import org.apache.spark.sql.functions.{asc, col, desc, grouping_id, max, min, sum}
 
 object Day30ExerciseJoin extends App {
   println("Ch8: Joins")
@@ -20,17 +22,44 @@ object Day30ExerciseJoin extends App {
 
   val joinExpression = dfSales.col("CustomerID") === dfCustomers.col("Id")
 
-  dfSales.join(dfCustomers, joinExpression).show()
+  val realPurchases = dfSales.join(dfCustomers, joinExpression)
 
   //same in spark sql
   spark.sql(
     """
       |SELECT * FROM dfTableSales JOIN dfTableCustomers
       |ON dfTableSales.CustomerID = dfTableCustomers.Id
+      |ORDER BY LastName DESC
       |""".stripMargin)
-    .show()
+    .show(false)
 
+  realPurchases
+    .cube("LastName", "InvoiceNo")
+    .agg(grouping_id(),sum("Quantity"), min("Quantity"), max("Quantity"))
+    .orderBy(desc("grouping_id()"))
+    .show(10)
 
+//  realPurchases
+//    .cube("LastName", "InvoiceNo")
+//    .agg(grouping_id(),sum("Quantity"), min("Quantity"), max("Quantity"))
+//    .orderBy(asc("grouping_id()"))
+//    .show()
+
+  realPurchases
+    .cube("InvoiceNo", "LastName")
+    .agg(grouping_id(),sum("Quantity"), min("Quantity"), max("Quantity"))
+    .orderBy(desc("grouping_id()"))
+    .show(10)
+
+  realPurchases.groupBy("StockCode")
+    .pivot("LastName")
+    .sum("Quantity")
+    .show(false)
+
+  realPurchases.groupBy("StockCode", "Description")
+    .pivot("LastName")
+    .sum("Quantity")
+    .show(false)
 
 }
 
